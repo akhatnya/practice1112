351d09931ee90194472e5972b7a38b4a23d06eae
Akhat <akhatnya@gmail.com>
2019-11-18 15:49:05 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var a = {
+    name: "Almas",
+    age: 16,
+    gpa: 1.9,
+    group: "IT-1911"
+}
+
+var b = {
+    name: "Nurlan",
+    age: 18,
+    gpa: 3.4,
+    group: "IT-1911"
+}
+
+var c = {
+    name: "Zhandaulet",
+    age: 17,
+    gpa: 1.1,
+    group: "IT-1912"
+}
+
+var arr = [a,b,c];
+
+console.log(arr);
